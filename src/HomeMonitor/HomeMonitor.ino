30fcd1496636f2916dffd82abfd8b1fdd94ee212
trm13 <trm13@aber.ac.uk>
2022-03-22 11:43:08 +0000

    Created string literal for HTML

--- src/HomeMonitor/HomeMonitor.ino
+++ src/HomeMonitor/HomeMonitor.ino
@@ -4,44 +4,39 @@
 #include <WiFiClient.h>
 #include <WebServer.h>
 
-const String page PROGMEM = "<head>"
-                            " <script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js\"></script>"
-                            " </head>"
-                            " <body>"
-                            " <h1>HomeMonitor: </h1><h2 id=\"text\">""</h2>\r\n"
-                            //                            " <h1>HomeMonitor: </h1><h2>Temperature:</h2> <h2 id=\"temp1\">""</h2>\r\n"
-                            //                            " <h2>Humidity:</h2><h2 id=\"hum1\">""</h2>\r\n"
-                            " <script>\r\n"
-                            " $(document).ready(function(){\r\n"
-                            " setInterval(getData,1000);\r\n"
-                            " function getData(){\r\n"
-                            " $.ajax({\r\n"
-                            "  type:\"GET\",\r\n"
-                            "  url:\"data\",\r\n"
-                            "  success: function(data){\r\n"
-                            //                            "  let s = JSON.parse(data);\r\n"
-                            //                            "  $('#temp1').html(s.temp);\r\n"
-                            //                            "  $('#hum1').html(s.hum);\r\n"
-                            "  $('#text').html(data);\r\n"
-                            "}\r\n"
-                            "}).done(function() {\r\n"
-                            "  console.log('ok');\r\n"
-                            "})\r\n"
-                            "}\r\n"
-                            "});"
-                            "</script>\r\n"
-                            "</body>";
+const char page[] PROGMEM = R"(
+<head>
+ <script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js\"></script>
+</head>
+<body>
+ <h1>HomeMonitor: </h1>
+ <h2 id=\"text\">""</h2>
+ <script>
+   $(document).ready(function(){
+     setInterval(getData,1000);
+     function getData(){
+       $.ajax({
+         type:\"GET\",
+         url:\"data\",
+         success: function(data){
+           let s = JSON.parse(data);
+           let parsedData = "";
+           $('#text').html(data);
+         }
+       }).done(function() {
+         console.log('ok');
+       })
+     }
+   });
+ </script>
+</body>
+)";
 
 WebServer server(80);
 
 const char* ssid = "trishypoo";
 const char* password = "gingerhair";
-String text = ""; //Data to send to web page.
-
-void handleRoot() {
-  String s = page; //Read HTML contents
-  server.send(200, "text/html", s); //Send web page
-}
+String text = "Collecting Data."; //Data to send to web page.
 
 House* house1 = new House("House 1");
 
@@ -50,6 +45,32 @@ void setup()
   // put your setup code here, to run once:
   Serial.begin(115200);
 
+  initialiseWebServer();
+
+  house1->addRoom(new Room("Bedroom"));
+  house1->getRoom("Bedroom")->addSensor(new Thermobeacon("Sensor1", "b5:70:00:00:06:c4"));
+  house1->getRoom("Bedroom")->addSensor(new Thermobeacon("Sensor2", "b5:70:00:00:07:db"));
+  house1->addRoom(new Room("Bathroom"));
+  house1->getRoom("Bathroom")->addSensor(new OneWireTempSensor("Sensor3", 23));
+
+  text = house1->toJSON();
+  Serial.println(text);
+
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+
+  server.handleClient();
+
+  //  text = house1->getRoom("Bedroom")->getData();
+  //  Serial.println(text);
+  //delay(2000);
+
+}
+
+void initialiseWebServer() {
   WiFi.mode(WIFI_STA); //Connect to your wifi
   WiFi.begin(ssid, password);
 
@@ -70,34 +91,15 @@ void setup()
   Serial.println(WiFi.localIP());  //IP address assigned to your ESP
   //----------------------------------------------------------------
 
-  server.on("/", handleRoot);      //This is display page
+  server.on("/", []() {
+    server.send(200, "text/html", page);
+  });
   server.on("/data", []() {
-    Serial.println(text);
+    //Serial.println(text);
     server.send(200, "text/plain", text);
-    text = "";
+    //text = "";
   });
 
   server.begin();                  //Start server
   Serial.println("HTTP server started");
-
-  house1->addRoom(new Room("Bedroom"));
-  house1->getRoom("Bedroom")->addSensor(new Thermobeacon("Sensor1", "b5:70:00:00:06:c4"));
-  house1->getRoom("Bedroom")->addSensor(new Thermobeacon("Sensor2", "b5:70:00:00:07:db"));
-  house1->addRoom(new Room("Bathroom"));
-  house1->getRoom("Bathroom")->addSensor(new OneWireTempSensor("Sensor3", 23));
-
-  text = house1->toJSON();
-  Serial.println(text);
-
-}
-
-void loop()
-{
-  // put your main code here, to run repeatedly:
-
-  //  text = house1->getRoom("Bedroom")->getData();
-  //  Serial.println(text);
-  //server.handleClient();
-  //delay(2000);
-
 }
